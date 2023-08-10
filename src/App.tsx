94198bfc46fbfa8b51f5c48b6128969712a908e9
Kristen Vincenti <kristen.vincenti17@gmail.com>
2023-08-09 22:57:34 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Simon Says</h1>
+      <p>This is a basic simon says game so I can learn how to code.</p>
+    </div>
+  );
+}
+
+export default App;
