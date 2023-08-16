49f91161d8a6671505a9cb6e945507b29e864a43
Kristen Vincenti <kristen.vincenti17@gmail.com>
2023-08-16 19:42:09 -0400

    Made new function that adds a random number to an array

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import './App.css';
 import GameButton from './components/GameButton/GameButton';
-import { Props } from './components/GameButton/types';
 
 function App() {
-
+  console.log(generateNewPattern([]));
   return (
     <div className="App">
       <h1>Simon Says</h1>
@@ -35,6 +34,13 @@ function App() {
 
 function onButtonClick(number: number) {
   console.log(number)
+}
+
+function generateNewPattern(currentPattern: number[]) {
+  const newPattern = [...currentPattern];
+  const ranNum = Math.ceil(Math.random() * 4);
+  newPattern.push(ranNum);
+  return newPattern;
 }
 
 export default App;
